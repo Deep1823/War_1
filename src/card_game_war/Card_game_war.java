29c730f43ab89858f4810d7c15c19259ac9acaeb
Deep1823 <pate4193@sheridancollege.ca>
2023-02-09 18:14:07 -0500

    In class exercise: 1 Commit Version 3.0.1

---
+++ src/card_game_war/Card_game_war.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package card_game_war;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Card_game_war {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a base code for war card game!!!");
+    }
+    
+}
